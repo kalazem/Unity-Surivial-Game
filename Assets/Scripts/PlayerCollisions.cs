e7e5b684d5e6c7e2f77f7fb5a021bd002ba40b95
khaled <kalddevv@gmail.com>
2020-10-28 01:53:43 +0000

    added basic collision detection and Ray casting

---
+++ Assets/Scripts/PlayerCollisions.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollisions : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       RaycastHit hit;
+        Debug.DrawRay(transform.position, transform.forward * 10);
+        if(Physics.Raycast (transform.position, transform.forward, out hit, 10))
+        {
+            if(hit.collider.gameObject.tag == "PlayerDoor")
+            {
+                Debug.Log("We are facing the door");
+            }
+        }
+        
+    }
+
+     void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+ 
+        if (hit.gameObject.tag == "PlayerDoor")
+        {
+            Debug.Log("Walked into the door");
+        }
+    }
+}
